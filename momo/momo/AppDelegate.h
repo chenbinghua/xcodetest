3d8e77899e76fb7cff50182caacb7383c6f159e5
chenbinghua <541787105@qq.com>
2015-07-20 19:40:37 +0800

    初始化项目

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by chenbinghua on 20/07/15.
+//  Copyright (c) 2015年 chenbinghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
